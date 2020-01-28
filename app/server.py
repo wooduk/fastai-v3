f4f1b4f855c5a5f0d8a651a58522d97e96f8db6a
Stephen Wood <wooduk@Stephens-MacBook-Air.local>
2020-01-28 11:43:26 +0100

    Added a message to show that we were loading the model

--- app/server.py
+++ app/server.py
@@ -30,7 +30,9 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
+    print('Downloading model + parameters')
     await download_file(export_file_url, path / export_file_name)
+    print('Done.')
     try:
         learn = load_learner(path, export_file_name)
         return learn
